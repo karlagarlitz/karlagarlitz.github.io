72224935df28cfacd5241e68d964e6a1af1641af
karlagarlitz <gar18054@byui.edu>
2020-01-11 19:55:24 -0600

    added js

---
+++ getdate.js
@@ -0,0 +1,20 @@
+let daynames= [
+    "Sunday",
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+    "Sunday"];
+
+let months= ["January", "February", "March", "April", "May","June","July","August","September","Obtober","November","December"];
+
+let d = new Date();
+let dayName = daynames[d.getDate()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+document.getElementById("currentdate").textContent = fulldate;
+
